68f0347a42b6c5a38af68be106aa30fdd1a7aca8
lethanh26061998 <lethanh26061998@gmail.com>
2019-10-28 17:39:00 +0700

    sua giao dien gio hang

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,10 @@ module.exports = {
                     }
                 ],
             },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: 'file-loader'
+            }
         ],
     },
     plugins: [
