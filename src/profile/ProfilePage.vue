902e303bf5b511ee07c37e34c137ff43fa5fb082
duongthoa <duongthoa98@gmail.com>
2019-11-06 00:12:36 +0700

    edit profile

--- src/profile/ProfilePage.vue
+++ src/profile/ProfilePage.vue
@@ -1,13 +1,55 @@
 <template>
-    <h1>Profile Page</h1>
+  <v-row>
+    <v-col cols="4" class="mx-auto"></v-col>
+    <v-col cols="8" class="mx-auto">
+      <v-container style="background-color: white">
+        <v-row>
+          <v-col cols="12">
+            <h2>Thông tin tài khoản</h2>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row>
+          <v-col cols="8">
+            <v-row>
+              <v-col cols="3" class="mx-auto">First Name</v-col>
+              <v-col cols="9" class="mx-auto"></v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3" class="mx-auto">Last Name</v-col>
+              <v-col cols="9" class="mx-auto"></v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3" class="mx-auto">Số điện thoại</v-col>
+              <v-col cols="9" class="mx-auto"></v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3" class="mx-auto">Email</v-col>
+              <v-col cols="9" class="mx-auto"></v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3" class="mx-auto">Địa chỉ</v-col>
+              <v-col cols="9" class="mx-auto"></v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3" class="mx-auto">Giới tính</v-col>
+              <v-col cols="9" class="mx-auto"></v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3" class="mx-auto">Ngày sinh</v-col>
+              <v-col cols="9" class="mx-auto"></v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="4" class="mx-auto"></v-col>
+        </v-row>
+      </v-container>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
 </style>
