623823a9639d557644f8b59930d460ced22939a1
duongthoa <duongthoa98@gmail.com>
2019-11-07 23:39:28 +0700

    edit login + register + profile

--- src/components/RegisterDialog.vue
+++ src/components/RegisterDialog.vue
@@ -11,138 +11,134 @@
       </v-toolbar>
       <v-card-text class="py-0">
         <v-container>
-          <v-row align="center" justify="center">
-            <v-col cols="12" md="5" class="py-0">
-              <ValidationProvider
-                name="first name"
-                rules="required|alpha"
-                v-slot="{ errors }"
-                :bails="false"
-              >
-                <v-text-field
-                  label="First name" 
-                  v-model="firstName"
-                  counter="16"
-                ></v-text-field>
-                <span class="red--text">{{ errors[0] }}</span>
-              </ValidationProvider>
-            </v-col>
-            <!-- <v-col cols="12" sm="6" md="4">
+          <ValidationObserver ref="observer" v-slot="{ invalid }" tag="form">
+            <v-row align="center" justify="center">
+              <v-col cols="12" md="5" class="py-0">
+                <ValidationProvider
+                  name="first name"
+                  rules="required|alpha"
+                  v-slot="{ errors }"
+                  :bails="false"
+                >
+                  <v-text-field label="First name" v-model="firstName" counter="16"></v-text-field>
+                  <span class="red--text">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </v-col>
+              <!-- <v-col cols="12" sm="6" md="4">
               <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-            </v-col> -->
-            <v-col cols="12" md="5" class="py-0" >
-              <ValidationProvider
-                name="last name"
-                rules="required|alpha"
-                v-slot="{ errors }"
-                :bails="false"
-              >
-                <v-text-field
-                  label="Last name" 
-                  v-model="lastName"
-                  counter="16"
-                ></v-text-field>
-                <span class="red--text">{{ errors[0] }}</span>
-              </ValidationProvider>
-            </v-col>
-          </v-row>
-          <v-row align="center" justify="center">
-            <v-col cols="12" md="10" class="py-0">
-              <v-menu
-                v-model="menudate"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on }">
-                  <v-text-field
-                    v-model="date"
-                    label="Birthday date"
-                    append-icon="mdi-calendar"
-                    readonly
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
-              </v-menu>
-            </v-col>
-            <!-- <v-col cols="12" md="5" class="py-0">
+              </v-col>-->
+              <v-col cols="12" md="5" class="py-0">
+                <ValidationProvider
+                  name="last name"
+                  rules="required|alpha"
+                  v-slot="{ errors }"
+                  :bails="false"
+                >
+                  <v-text-field label="Last name" v-model="lastName" counter="16"></v-text-field>
+                  <span class="red--text">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+            <v-row align="center" justify="center">
+              <v-col cols="12" md="10" class="py-0">
+                <v-menu
+                  v-model="menudate"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      v-model="date"
+                      label="Birthday date"
+                      append-icon="mdi-calendar"
+                      readonly
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+                </v-menu>
+              </v-col>
+              <!-- <v-col cols="12" md="5" class="py-0">
               <v-autocomplete
                 :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
                 label="Address"
                 
               ></v-autocomplete>
-            </v-col> -->
-          </v-row>
-          <v-row>
-            <v-col cols="12" md="10" class="mx-auto py-0">
-              <ValidationProvider
-                name="email"
-                rules="required|email"
-                v-slot="{ errors }"
-                :bails="false"
-              >
-                <v-text-field
-                  prepend-icon="mdi-email" 
-                  label="Email" 
-                  v-model="email"
-                ></v-text-field>
-                <span class="red--text">{{ errors[0] }}</span>
-              </ValidationProvider>
+              </v-col>-->
+            </v-row>
+            <v-row>
+              <v-col cols="12" md="10" class="mx-auto py-0">
+                <ValidationProvider
+                  name="email"
+                  rules="required|email"
+                  v-slot="{ errors }"
+                  :bails="false"
+                >
+                  <v-text-field prepend-icon="mdi-email" label="Email" v-model="email"></v-text-field>
+                  <span class="red--text">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </v-col>
+              <v-col cols="12" md="10" class="mx-auto py-0">
+                <ValidationProvider
+                  name="password"
+                  rules="required|min:8"
+                  v-slot="{ errors }"
+                  :bails="false"
+                >
+                  <v-text-field
+                    :type="showPassword ? 'text' : 'password'"
+                    label="Password"
+                    v-model="password"
+                    counter="16"
+                    prepend-icon="mdi-lock"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword"
+                  ></v-text-field>
+                  <span class="red--text">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </v-col>
+              <v-col cols="12" md="10" class="mx-auto py-0">
+                <ValidationProvider
+                  name="confirm password"
+                  rules="required|min:8|confirmed:password"
+                  v-slot="{ errors }"
+                  :bails="false"
+                >
+                  <v-text-field
+                    :type="showConfirmPassword ? 'text' : 'password'"
+                    label="Confirm Password"
+                    v-model="confirmPassword"
+                    counter="16"
+                    prepend-icon="mdi-lock"
+                    :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showConfirmPassword = !showConfirmPassword"
+                    @keyup.enter="submit"
+                  ></v-text-field>
+                  <span class="red--text">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+            <v-col cols="12" md="6" class="mx-auto pt-0">
+              <v-card-actions>
+                <v-btn
+                  color="primary"
+                  class="layout justify-center"
+                  @click="submit"
+                  :disabled="invalid"
+                >Register</v-btn>
+              </v-card-actions>
             </v-col>
-            <v-col cols="12" md="10" class="mx-auto py-0">
-              <ValidationProvider
-                name="password"
-                rules="required|min:8"
-                v-slot="{ errors }"
-                :bails="false"
-              >
-                <v-text-field 
-                  :type="showPassword ? 'text' : 'password'" 
-                  label="Password"
-                  v-model="password"
-                  counter="16"
-                  prepend-icon="mdi-lock"
-                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="showPassword = !showPassword"
-                ></v-text-field>
-                <span class="red--text">{{ errors[0] }}</span>
-              </ValidationProvider>
-            </v-col>
-            <v-col cols="12" md="10" class="mx-auto py-0">
-              <ValidationProvider
-                name="confirm password"
-                rules="required|min:8|confirmed:password"
-                v-slot="{ errors }"
-                :bails="false"
-              >
-                <v-text-field 
-                  :type="showConfirmPassword ? 'text' : 'password'" 
-                  label="Confirm Password"
-                  v-model="confirmPassword"
-                  counter="16"
-                  prepend-icon="mdi-lock"
-                  :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="showConfirmPassword = !showConfirmPassword"
-                ></v-text-field>
-                <span class="red--text">{{ errors[0] }}</span>
-              </ValidationProvider>
-            </v-col>
-          </v-row>
+          </ValidationObserver>
         </v-container>
       </v-card-text>
-      <v-col cols="12" md="6" class="mx-auto pt-0">
-        <v-card-actions>
-          <v-btn color="primary" class="layout justify-center" @click="register">Register</v-btn>
-        </v-card-actions>
-      </v-col>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
   data() {
@@ -157,22 +153,28 @@ export default {
       date: null,
       menudate: false,
       showPassword: false,
-      showConfirmPassword: false,
+      showConfirmPassword: false
     };
   },
-  computed: {
-    ...mapState({
-      loginStatus: state => state.login.loginStatus
-    })
-  },
   methods: {
-    register() {
-        this.dialog = false
+    async submit() {
+      try {
+        const isValid = await this.$refs.observer.validate();
+        this.$store.dispatch("alert/addAlert", {
+          type: "success",
+          message: "Register successfully!"
+        });
+        this.dialog = false;
+      } catch (error) {
+        this.$store.dispatch("alert/addAlert", {
+          type: "error",
+          message: "Register error!"
+        });
+      }
     }
   }
 };
 </script>
 
 <style scoped>
-
 </style>
