018fe1b2583286b9f3b2d1e40d0dbbadd691ed15
Doan Duy Dat <doanduydat98@gmail.com>
2019-11-18 22:19:38 +0700

    add menu for icon user

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -17,12 +17,33 @@
     <cart-dialog></cart-dialog>
     <!-- </template> -->
 
-    <v-btn icon v-if="loggedIn" @click="logout">
-        <v-icon>mdi-account-circle</v-icon>
-    </v-btn>
+    <v-menu v-if="loggedIn" open-on-hover bottom offset-y transition="slide-y-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on"  depressed color='blue'>
+          <v-icon left>mdi-account-circle </v-icon>User Name
+          
+        </v-btn>
+        
+      </template>
+
+      <v-list>
+        <v-list-item to="/account/profile">
+          <v-list-item-title>Profile</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/account/history">
+          <v-list-item-title>Lịch sử đặt hàng</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="logout">
+          <v-list-item-title>Log Out</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <!-- <v-btn icon @click="logout">
+      <v-icon>mdi-account-circle</v-icon>
+    </v-btn>-->
 
     <!-- <template v-if="!loginStatus && $vuetify.breakpoint.smAndUp"> -->
-    <template v-else>
+    <template v-if="!loggedIn">
       <login-dialog></login-dialog>
       <register-dialog></register-dialog>
     </template>
@@ -47,7 +68,9 @@ export default {
     CartDialog
   },
   data() {
-    return {};
+    return {
+      
+    };
   },
 
   computed: {
