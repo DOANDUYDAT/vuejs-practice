d2c688b9a50b2dfd2b4b00f89dcc9098acf2281c
lethanh26061998 <lethanh26061998@gmail.com>
2019-12-02 19:34:51 +0700

    add logo to The Header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,56 +1,62 @@
 <template>
-  <v-app-bar app  dark >
+  <v-app-bar app dark>
     <!-- <template v-slot:img="{ props }">
       <v-img v-bind="props" gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"></v-img>
-    </template> -->
-
-    <v-app-bar-nav-icon class="d-block d-sm-none"></v-app-bar-nav-icon>
-
-    <v-toolbar-title>BK Mobile</v-toolbar-title>
-
-    <div class="flex-grow-1"></div>
-
-    <search-box></search-box>
-
-    <v-btn icon to="/home" exact>
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
-
-    <cart-dialog></cart-dialog>
-
-    <v-menu v-if="loggedIn" open-on-hover bottom offset-y transition="slide-y-transition">
-      <template v-slot:activator="{ on }">
-        <v-btn v-on="on" depressed color="it-blue-lighten" to="/account/profile" exact>
-          {{ fullName }}
-          <v-icon right>mdi-account-circle</v-icon>
+    </template>-->
+    <v-container class="px-0">
+      <v-row align="center" no-gutters>
+        <v-col cols="3">
+          <v-avatar size="150">
+            <v-img :src="require('@/_assets/image/logo.png')"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col cols="5">
+          <search-box></search-box>
+        </v-col>
+        <v-spacer></v-spacer>
+        <!-- <v-col cols="4"> -->
+        <v-btn icon to="/home" exact>
+          <v-icon>mdi-home</v-icon>
         </v-btn>
-      </template>
 
-      <v-list>
-        <v-list-item to="/account/profile">
-          <v-list-item-title>Profile</v-list-item-title>
-        </v-list-item>
-        <v-list-item to="/account/history">
-          <v-list-item-title>Lịch sử đặt hàng</v-list-item-title>
-        </v-list-item>
-        <v-list-item @click="logout">
-          <v-list-item-title>Log Out</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-    <!-- <v-btn icon @click="logout">
+        <cart-dialog></cart-dialog>
+
+        <v-menu v-if="loggedIn" open-on-hover bottom offset-y transition="slide-y-transition">
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" depressed color="it-blue-lighten" to="/account/profile" exact>
+              {{ fullName }}
+              <v-icon right>mdi-account-circle</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item to="/account/profile">
+              <v-list-item-title>Profile</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/account/history">
+              <v-list-item-title>Lịch sử đặt hàng</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="logout">
+              <v-list-item-title>Log Out</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <!-- <v-btn icon @click="logout">
       <v-icon>mdi-account-circle</v-icon>
-    </v-btn>-->
+        </v-btn>-->
 
-    <!-- <template v-if="!loginStatus && $vuetify.breakpoint.smAndUp"> -->
-    <template v-if="!loggedIn">
-      <login-dialog></login-dialog>
-      <register-dialog></register-dialog>
-    </template>
+        <!-- <template v-if="!loginStatus && $vuetify.breakpoint.smAndUp"> -->
+        <template v-if="!loggedIn">
+          <login-dialog></login-dialog>
+          <register-dialog></register-dialog>
+        </template>
 
-    <!-- <v-btn icon>
+        <!-- <v-btn icon>
       <v-icon>mdi-dots-vertical</v-icon>
-    </v-btn>-->
+        </v-btn>-->
+        <!-- </v-col> -->
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
 
