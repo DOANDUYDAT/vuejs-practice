edbaea41249f46c5221de0bfb14d63e414aafddb
Doan Duy Dat <doanduydat98@gmail.com>
2019-12-14 22:32:47 +0700

    fix: header of admin

--- src/admin/components/TheHeader.vue
+++ src/admin/components/TheHeader.vue
@@ -16,29 +16,81 @@
       class="hidden-sm-and-down"
     />
     <v-spacer />
-    <v-btn icon>
-      <v-icon>mdi-apps</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-bell</v-icon>
-    </v-btn>
-    <v-btn icon large>
-      <v-avatar size="32px" item>
-        <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" />
-      </v-avatar>
-    </v-btn>
+    <v-menu v-if="loggedIn" open-on-hover bottom offset-y transition="slide-y-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" depressed color="gg-red" to="/account/profile" exact>
+          {{ fullName }}
+          <v-icon right>mdi-account-circle</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item to="/account/profile">
+          <v-list-item-title>Profile</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/home">
+          <v-list-item-title>Trang chủ</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="logout">
+          <v-list-item-title>Log Out</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
+import { userService } from "@/_api";
+import { mapState, mapActions } from "vuex";
+import _ from "lodash";
+
 export default {
   data() {
-    return {};
+    return {
+      userInfo: {}
+    };
+  },
+  computed: {
+    ...mapState({
+      loggedIn: state => state.authentication.status.loggedIn,
+      user: state => state.authentication.user
+    }),
+    fullName() {
+      return this.userInfo.firstName + " " + this.userInfo.lastName;
+    }
   },
   methods: {
+    ...mapActions({
+      logoutAction: "authentication/logout"
+    }),
+    async logout() {
+      try {
+        const isSuccess = await this.logoutAction();
+        if (isSuccess) {
+          this.$store.dispatch("alert/success", {
+            message: "You are log out!"
+          });
+          const currentPath = this.$route.path;
+          if (currentPath !== "/home") {
+            this.$router.push({ path: "/home" });
+          }
+        }
+      } catch (error) {
+        if (error.response) {
+          this.$store.dispatch("alert/error", {
+            message: error.response.data.message
+          });
+        }
+      }
+    },
+    async getData() {
+      this.userInfo = await userService.getProfile();
+    },
     changeDrawer() {
       this.$store.dispatch("navigator/changeDrawer", { value: true });
     }
+  },
+  created() {
+    this.getData();
   }
 };
 </script>
