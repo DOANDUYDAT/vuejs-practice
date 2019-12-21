7b83f40d85ace40bd7b37295db2075d2aae94835
lethanh26061998 <lethanh26061998@gmail.com>
2019-12-21 16:21:44 +0700

    fix footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,62 +1,54 @@
 <template>
-  
-    <v-footer
-      absolute
-      padless
-      app
-      dark
-    >
-      <v-card
-        flat
-        tile
-        width="100%"
-        class="text-center"
-      >
-        <v-card-text>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4"
-            icon
-          >
-            <v-icon size="24px">{{ icon }}</v-icon>
-          </v-btn>
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-text class="white--text">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
-
-    
-  
+  <v-footer absolute padless app dark>
+    <v-card flat tile width="100%">
+      <v-row no-gutters>
+        <v-col cols="6" class="text-center">
+          <v-card-text>
+            <b>Công Ty Cổ Phần Thương Mại - Dịch Vụ Bách Khoa</b>
+          </v-card-text>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <v-card-text>
+            <b>Địa chỉ: Số 1, Đại Cồ Việt, Hai Bà Trưng, Hà Nội</b>
+          </v-card-text>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col cols="6" class="text-center">
+          <v-card-text>© 2016 - 2019 /GPĐKKD số 0123456 do Sở KHĐT TP.HN cấp</v-card-text>
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <v-card-text>Hotline: 01238888</v-card-text>
+        </v-col>
+      </v-row>
+      <!-- <v-card-text class="white--text">
+        2016 - {{ new Date().getFullYear() }}
+        <span></span>GPĐKKD số 012345678 do Sở KHĐT TP.HN cấp
+      </v-card-text>-->
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      icons: [
-        'mdi-home',
-        'mdi-email',
-        'mdi-calendar',
-        'mdi-delete',
-      ],
-      items: [
-        'default',
-        'absolute',
-        'fixed',
-        'app'
-      ],
-      padless: true,
-      variant: 'default',
-    }),
-    computed: {
-      
-    },
-  }
+export default {
+  data: () => ({
+    // icons: [
+    //   'mdi-home',
+    //   'mdi-email',
+    //   'mdi-calendar',
+    //   'mdi-delete',
+    // ],
+    // items: [
+    //   'default',
+    //   'absolute',
+    //   'fixed',
+    //   'app'
+    // ],
+    padless: true,
+    variant: "default"
+  }),
+  computed: {}
+};
 </script>
 
 <style lang="scss" scoped>
